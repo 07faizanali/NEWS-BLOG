6688663f508bc05a77a9f102247f1bff8ac2683c
Faizan Ali <mohd.faizanali.m.z@gmail.com>
2024-03-25 02:40:06 +0530

    2nd commit

--- src/components/NewsBoard.jsx
+++ src/components/NewsBoard.jsx
@@ -3,10 +3,12 @@ import NewsItem from './NewsItem';
 
 const NewsBoard = ({category}) => {
 
+  
+
   const [articles, setArticles] = useState([]);
     
     useEffect(()=>{
-      let url=`https://newsapi.org/v2/top-headlines?country=us&category=${category}&apiKey=e12bf53ca5dc42939f30b38a2a8e69c9`;
+      let url=`https://newsapi.org/v2/top-headlines?country=in&category=${category}&apiKey=e12bf53ca5dc42939f30b38a2a8e69c9`;
       fetch(url).then(response=> response.json()).then(data=> setArticles(data.articles));
     },[category])
   
